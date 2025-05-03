3eb0a5b6977e9033e76fa4c7c85eb6f6ed25288d
Sidharth-Singh10 <70999945+Sidharth-Singh10@users.noreply.github.com>
2025-05-03 22:37:06 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Rust
+# cargo build --release
+
+# C/C++
+# gcc -O3 -DNDEBUG -o main main.c
+
+# Go
+# GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o app main.go
